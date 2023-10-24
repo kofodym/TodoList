a89e2e013dea1830314ec6d06cd602977b415546
KofoCole <kofodym07@yahoo.com>
2023-10-24 15:12:05 +0100

    added a js file and select the form and textbox

---
+++ Resources/js/script.js
@@ -0,0 +1,4 @@
+//1select the form  and textbox
+const formFill = document.querySelector(".form");
+const inputText = document.querySelector(".input-text");
+// const buttonAdd = document.querySelector("#submit");
