b15ac053f0c02a7b92fea41ecfb0ef48bdb6d559
KofoCole <kofodym07@yahoo.com>
2023-10-24 15:13:51 +0100

    Add an event to the form, when you click on the button with the submit-type it triggers the event form

--- Resources/js/script.js
+++ Resources/js/script.js
@@ -2,3 +2,11 @@
 const formFill = document.querySelector(".form");
 const inputText = document.querySelector(".input-text");
 // const buttonAdd = document.querySelector("#submit");
+
+ //2 Add an event to the form, when you click on the button with the "submit type" it triggers the event form.
+formFill.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //test it by console logging the value of the input
+  //   console.log(inputText.value);
+  
+});
